e1f217797e1374d8f0b959c3afb42469b45888a2
xrlin <professerlin@gmail.com>
2016-10-16 09:29:01 +0800

    Change categories component style

--- app/assets/javascripts/admin/src/components/Categories.vue
+++ app/assets/javascripts/admin/src/components/Categories.vue
@@ -1,55 +1,70 @@
 <template>
-  <template v-for="c in categories">
-    <div class="pure-form pure-form-stacked">
-      <div class="pure-g">
-        <div class="pure-u-1 pure-u-md-1-3">
-          <label for="email">Name</label>
-          <input name="name" class="pure-u-23-24" type="text" v-model="c.name" required>
-        </div>
-        <div class="pure-u-1 pure-u-md-1-3">
-          <label for="email">Slug</label>
-          <input name="slug" class="pure-u-23-24" type="text" v-model="c.slug">
-        </div>
-      </div>
-      <button class="pure-button-primary" @click="updateCategory(c)">Update</button>
-      <button class="pure-button-primary" @click="destroyCategory(c)">Delete</button>
-    </div>
-  </template>
-  <div class="pure-form pure-form-stacked">
-    <div class="pure-g">
-      <div class="pure-u-1 pure-u-md-1-3">
-        <label for="email">Name</label>
-        <input id="name" class="pure-u-23-24" type="text" v-model="new_obj.name" required>
-      </div>
-      <div class="pure-u-1 pure-u-md-1-3">
-        <label for="email">Slug</label>
-        <input id="slug" class="pure-u-23-24" v-model="new_obj.slug" type="text">
-      </div>
-    </div>
-    <button class="pure-button-primary" type="submit" @click="createCategory(new_obj)">Create</button>
-  </div>
+  <header class="pure-u-1"><h3>Categories</h3></header>
+  <table class="pure-table pure-form pure-u custom-table">
+    <thead>
+      <tr>
+          <th>Name</th>
+          <th>Slug</th>
+          <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <template v-for="c in categories">
+        <tr>
+          <td><input name="name" class="pure-u-23-24" type="text" v-model="c.name" required></td>
+          <td><input name="slug" class="pure-u-23-24" type="text" v-model="c.slug"></td>
+          <td>
+            <button class="pure-button-primary" @click="updateCategory(c)">Update</button>
+            <button class="pure-button-primary" @click="destroyCategory(c)">Delete</button>
+          </td>
+        </tr>
+      </template>
+      <tr>
+        <td><input name="name" class="pure-u-23-24" type="text" v-model="new_obj.name" required></td>
+        <td><input name="slug" class="pure-u-23-24" type="text" v-model="new_obj.slug"></td>
+        <td>
+          <button class="pure-button-primary" @click="createCategory(new_obj)">Create</button>
+        </td>
+    </tbody>
+  </table>
 </template>
 
 <script>
-  import { categories } from '../vuex/getters.js'
-  import { createCategory, updateCategory,destroyCategory } from '../vuex/actions.js'
-  import categories_api from '../api/categories.js'
+import { categories } from '../vuex/getters.js'
+import { createCategory, updateCategory,destroyCategory } from '../vuex/actions.js'
+import categories_api from '../api/categories.js'
 
-  export default {
-    data: function(){
-      return {
-        new_obj: {name: null, slug: null}
-      }
+export default {
+  data: function(){
+    return {
+      new_obj: {name: null, slug: null}
+    }
+  },
+  vuex: {
+    getters: {
+      categories
     },
-    vuex: {
-      getters: {
-        categories
-      },
-      actions: {
-        createCategory,
-        updateCategory,
-        destroyCategory
-      }
+    actions: {
+      createCategory,
+      updateCategory,
+      destroyCategory
     }
   }
+}
 </script>
+
+<style scoped>
+.custom-table {
+  border: none;
+}
+.custom-table td, .custom-table th {
+  border: none;
+}
+thead {
+  background: none;
+}
+header {
+  padding-left: 1em;
+  border-bottom: 1px solid #c9d8db;
+}
+</style>
